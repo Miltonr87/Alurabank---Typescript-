58f6ee9a326f94e87e07979c0a8410f28a7967b1
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-09-01 21:34:59 -0300

    Added private modificator
    As a security and blocking of variables that are no longer accessible with Typescript.

--- app/app.ts
+++ app/app.ts
@@ -2,12 +2,12 @@ import { Negociacao } from './models/negociacao.js';
 
 const negociacao = new Negociacao (new Date(), 10, 100);
 console.log(negociacao.volume);
-alert('Hello Typescript')
+alert('Hello Typescript');
 
-
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 // yarn run server - para iniciar o servidor. Opção setada previamente no package.json
 // yarn run compile - para verificar erros antes de compilar
 // yarn run watch - coloca o modo de observação em mudanças nos arquivos
 // yarn run start - para iniciar o compilador do Typescript e permitir alterações automáticas
 // Tudo em "Dist" é compilado do TS seguindo a mesma árvore criada na aba de models!
+////////////////////////////////////////////////////////////////////////////////////////////////
