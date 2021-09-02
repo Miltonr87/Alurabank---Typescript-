2f04b49938f69da382e66ea116d12292d0faa048
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-09-01 21:27:40 -0300

    automatizando a compilação

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,13 @@
 import { Negociacao } from './models/negociacao.js';
 
-const negociacao = new Negociacao (new Date());
+const negociacao = new Negociacao (new Date(), 10, 100);
 console.log(negociacao.volume);
+alert('Hello Typescript')
 
 
-
-// yarn run server - para startar
+/////////////////////////////////////////////////////////////////////////////
+// yarn run server - para iniciar o servidor. Opção setada previamente no package.json
 // yarn run compile - para verificar erros antes de compilar
+// yarn run watch - coloca o modo de observação em mudanças nos arquivos
+// yarn run start - para iniciar o compilador do Typescript e permitir alterações automáticas
+// Tudo em "Dist" é compilado do TS seguindo a mesma árvore criada na aba de models!
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,9 @@
 import { Negociacao } from './models/negociacao.js';
-const negociacao = new Negociacao(new Date());
+const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
-// yarn run server
+alert('Hello Typescript');
+/////////////////////////////////////////////////////////////////////////////
+// yarn run server - para startar. Opção setada previamente no package.json
+// yarn run compile - para verificar erros antes de compilar
+// yarn run watch - coloca o modo de observação em mudanças nos arquivos
+// Tudo em "Dist" é compilado do TS em "Models" para Javascript seguindo a mesma árvore criada na aba de models!
