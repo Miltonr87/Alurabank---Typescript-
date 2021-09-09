81cbf84600cfa317c2a82ef913d0ff0bf025b6ff
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-09-08 22:02:03 -0300

    comments

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,8 +1,11 @@
 import { Negociacao } from "./negociacao";
 
 export class Negociacoes {                                          
-    // Array que recebe todo e somente o componente visto em negociação. Ele também poderia receber tipos primitivos como <string> ou mesmo <boolean> 
+    // Array que recebe todo e somente o componente visto em negociação. Ele também poderia receber tipos primitivos como <string> ou mesmo <boolean> . Esse recurso é chamado de diamante <>
+    // também poderia ser feito assim --> private negociacoes: Negociacao[] = [] 
+    
     private negociacoes: Array<Negociacao> = [];
+    
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,6 +1,7 @@
 export class Negociacoes {
     constructor() {
-        // Array que recebe todo e somente o componente visto em negociação. Ele também poderia receber tipos primitivos como <string> ou mesmo <boolean> 
+        // Array que recebe todo e somente o componente visto em negociação. Ele também poderia receber tipos primitivos como <string> ou mesmo <boolean> . Esse recurso é chamado de diamante <>
+        // também poderia ser feito assim --> private negociacoes: Negociacao[] = [] 
         this.negociacoes = [];
     }
     adiciona(negociacao) {
